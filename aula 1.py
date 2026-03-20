33fd381b027cd6929698ca078daa23d330adc4cb
lozlau <lozlau@gmail.com>
2026-03-20 19:50:17 -0300

    versao 1.0.0

---
+++ aula 1.py
@@ -0,0 +1,3 @@
+print("Sejam bem-vindos ao mundo da programação")
+dado = input("Digite o seu nome: ")
+print("Você digitou: ", dado)
---
+++ aula 1a.py
@@ -0,0 +1,5 @@
+
+# Exercícios Práticos - cálculos
+metro = 1
+polegada = metro * 39.3700787
+print(str(metro) + " metro(s) equivale(m) a " + str(polegada) + " polegada(s)")
